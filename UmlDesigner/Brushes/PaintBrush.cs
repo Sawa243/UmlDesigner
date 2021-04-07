e4917c08e839e80af2f61be177d8221ed6e48e87
Eugene <Zhekul.90@gmail.com>
2021-04-08 00:06:52 +0300

    add PaintBrush

---
+++ UmlDesigner/Brushes/PaintBrush.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmlDesigner
+{
+    public class PaintBrush
+    {
+        protected Pen _pen;
+        public Point point { get; set; }
+
+        public PaintBrush()
+        {
+            _pen = new Pen(Color.Red, 44);
+        }
+
+        public void Draw(Graphics graphics, Bitmap bitmap)
+        {
+            bitmap.SetPixel(point.X, point.Y, _pen.Color);
+        }
+
+    }
+}
