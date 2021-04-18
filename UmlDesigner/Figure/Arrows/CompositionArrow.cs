d592a2aaf0823ffb063bc019bd66c305d9dfb162
Eugene <Zhekul.90@gmail.com>
2021-04-18 16:52:27 +0300

    add CompositionArrow

---
+++ UmlDesigner/Figure/Arrows/CompositionArrow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UmlDesigner.Figure.Action;
+
+namespace UmlDesigner.Figure.Arrows
+{
+    public class CompositionArrow : AbstractArrow
+    {
+        public CompositionArrow(Pen pen, IAction action)
+        {
+            action = action;
+            _pen = pen;
+            _pen.EndCap = LineCap.DiamondAnchor;
+
+        }
+    }
+}
