905126632442d0566b04ab8793b56df38a2759e9
Eugene <Zhekul.90@gmail.com>
2021-04-20 20:53:54 +0300

    rename AssotiationFabric ->  AssotiationFactory

---
+++ UmlDesigner/Fabric/AssotiationFactory.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Drawing;
+using UmlDesigner.Figure;
+using UmlDesigner.Figure.Action;
+using UmlDesigner.Figure.Forms;
+
+namespace UmlDesigner.Fabric
+{
+    public class AssotiationFactory : IFactory
+    {
+        public AbstractAllFigurs GetElement(Pen pen)
+        {
+            return new AssotiationArrow(pen, new Drawing());
+        }
+    }
+}
