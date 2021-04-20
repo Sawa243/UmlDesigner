4193650559155d1dc43451b506e79a323f23cd7d
Eugene <Zhekul.90@gmail.com>
2021-04-20 15:25:46 +0300

    add IsItYou and Move AbstractAllFigurs

--- UmlDesigner/Figure/AbstractAllFigurs.cs
+++ UmlDesigner/Figure/AbstractAllFigurs.cs
@@ -21,6 +21,53 @@ namespace UmlDesigner.Figure
         protected abstract List<Point> GetPoints();
 
         public abstract void  Draw(Graphics graphics);
-       
+
+        public bool IsItYou(Point point)
+        {
+            int xMax;
+            int xMin;
+            int yMax;
+            int yMin;
+
+            if (StartPoint.X > EndPoint.X)
+            {
+                xMax = StartPoint.X;
+                xMin = EndPoint.X;
+            }
+            else
+            {
+                xMin = StartPoint.X;
+                xMax = EndPoint.X;
+            }
+
+            if (StartPoint.Y > EndPoint.Y)
+            {
+                yMax = StartPoint.Y;
+                yMin = EndPoint.Y;
+            }
+            else
+            {
+                yMin = StartPoint.Y;
+                yMax = EndPoint.Y;
+            }
+
+            if (point.X <= xMax && point.X >= xMin
+             && point.Y <= yMax && point.Y >= yMin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Move(int deltaX, int deltaY)
+        {
+            StartPoint = new Point(StartPoint.X + deltaX, StartPoint.Y + deltaY);
+            EndPoint = new Point(EndPoint.X + deltaX, EndPoint.Y + deltaY);
+
+        }
+
     }
 }
