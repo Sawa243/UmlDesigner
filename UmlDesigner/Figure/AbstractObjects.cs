d4e30e8bb711afd0587f4f59ad5666b600730d91
Eugene <Zhekul.90@gmail.com>
2021-04-22 16:34:10 +0300

    add AbstractObjects IsItMe, Move

--- UmlDesigner/Figure/AbstractObjects.cs
+++ UmlDesigner/Figure/AbstractObjects.cs
@@ -9,7 +9,7 @@ using UmlDesigner.Figure.Forms;
 
 namespace UmlDesigner.Figure
 {
-   public abstract class AbstractObjects: AbstractAllFigurs
+    public abstract class AbstractObjects : AbstractAllFigurs
     {
         protected string _nameForm;
         public AbstractObjects()
@@ -26,7 +26,51 @@ namespace UmlDesigner.Figure
         }
         public override void Draw(Graphics graphics)
         {
-            _action.Draw(graphics,_pen, GetPoints());
+            _action.Draw(graphics, _pen, GetPoints());
+        }
+
+        public override bool IsItMe(Point point)
+        {
+            int xMax;
+            int xMin;
+            int yMax;
+            int yMin;
+
+            if (StartPoint.X > EndPoint.X)
+            {
+                xMax = StartPoint.X;
+                xMin = EndPoint.X;
+            }
+            else
+            {
+                xMin = StartPoint.X;
+                xMax = EndPoint.X;
+            }
+
+            if (StartPoint.Y > EndPoint.Y)
+            {
+                yMax = StartPoint.Y;
+                yMin = EndPoint.Y;
+            }
+            else
+            {
+                yMin = StartPoint.Y;
+                yMax = EndPoint.Y;
+            }
+            if (point.X <= xMax && point.X >= xMin
+             && point.Y <= yMax && point.Y >= yMin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override void Move(int deltaX, int deltaY)
+        {
+            StartPoint = new Point(StartPoint.X + deltaX, StartPoint.Y + deltaY);
+            EndPoint = new Point(EndPoint.X + deltaX, EndPoint.Y + deltaY);
         }
     }
 }
