1f653734cade1fcb7648051ae61777f8c8f2a474
Eugene <Zhekul.90@gmail.com>
2021-04-22 16:33:09 +0300

    make AbstractAllFigurs abstract IsItMe, Move

--- UmlDesigner/Figure/AbstractAllFigurs.cs
+++ UmlDesigner/Figure/AbstractAllFigurs.cs
@@ -8,7 +8,7 @@ using UmlDesigner.Figure.Action;
 
 namespace UmlDesigner.Figure
 {
-   public abstract class AbstractAllFigurs
+    public abstract class AbstractAllFigurs
     {
         protected IAction _action;
         protected Pen _pen;
@@ -20,52 +20,8 @@ namespace UmlDesigner.Figure
             _action = new Drawing();
         }
         protected abstract List<Point> GetPoints();
-        public abstract void  Draw(Graphics graphics);
-
-        public bool IsItMe(Point point)
-        {
-            int xMax;
-            int xMin;
-            int yMax;
-            int yMin;
-
-            if (StartPoint.X > EndPoint.X)
-            {
-                xMax = StartPoint.X;
-                xMin = EndPoint.X;
-            }
-            else
-            {
-                xMin = StartPoint.X;
-                xMax = EndPoint.X;
-            }
-
-            if (StartPoint.Y > EndPoint.Y)
-            {
-                yMax = StartPoint.Y;
-                yMin = EndPoint.Y;
-            }
-            else
-            {
-                yMin = StartPoint.Y;
-                yMax = EndPoint.Y;
-            }
-            if (point.X <= xMax && point.X >= xMin
-             && point.Y <= yMax && point.Y >= yMin)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public void Move(int deltaX, int deltaY)
-        {
-            StartPoint = new Point(StartPoint.X + deltaX, StartPoint.Y + deltaY);
-            EndPoint = new Point(EndPoint.X + deltaX, EndPoint.Y + deltaY);
-        }
-
+        public abstract void Draw(Graphics graphics);
+        public abstract bool IsItMe(Point point);
+        public abstract void Move(int deltaX, int deltaY);
     }
 }
